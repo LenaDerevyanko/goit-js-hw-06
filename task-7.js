1ae9782f514fca47cf1db7ad60458e6de21b01ef
LenaDerevyanko <106963708+LenaDerevyanko@users.noreply.github.com>
2022-10-09 23:10:49 +0300

    up

--- task-7.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector('#font-size-control');
-let spanEl = document.querySelector('#text');
-spanEl.style.fontSize = '0px';
-
-const editValue = () => {
-  spanEl.style.fontSize = `${inputEl.value}px`;
-};
-
-inputEl.addEventListener('input', editValue);
