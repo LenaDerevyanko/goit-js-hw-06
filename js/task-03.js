7e56633ddc595a96a8e9996ffa25df3a8dbfe2b0
LenaDerevyanko <106963708+LenaDerevyanko@users.noreply.github.com>
2022-10-12 10:34:53 +0300

    up

--- js/task-03.js
+++ js/task-03.js
@@ -15,12 +15,11 @@ const images = [
 
 const list = document.querySelector('.gallery');
 let markup = '';
-images.forEach(img => {
-  markup = images
-    .map(
+
+  markup = images.map(
       img =>
         `<li class="gallery__item"><img class="gallery__img" src="${img.url}" alt="${img.alt}"></li>`
     )
     .join('');
-});
+
 list.insertAdjacentHTML('afterbegin', markup);
--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,15 @@
 const btnRef = document.querySelector('.change-color');
 const styleWindowRef = document.querySelector('body');
 const codeColorRef = document.querySelector('.color');
-btnRef.addEventListener('click', ()=>getRandomHexColor());
-function getRandomHexColor() {
-  const newColor = "#" + Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0);
-  console.log(newColor);
-  return newColor;
-} 
-console.log(btnRef);
+btnRef.addEventListener('click', () => {
+  const colors = getRandomHexColor()
+  document.body.style.backgroundColor = colors
+  codeColorRef.textContent = colors
+})
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`
+  };
+
